103d1bdd374e272acd3cbdf21fc20b95fa783d93
Matheus Alves <57298342+Matheus-0@users.noreply.github.com>
2023-08-04 14:35:17 -0300

    Consertando hash das senhas nas migrações.

--- water_server/src/main/resources/db/migration/V6__Insert_Data_In_Users.sql
+++ water_server/src/main/resources/db/migration/V6__Insert_Data_In_Users.sql
@@ -1,3 +1,3 @@
 INSERT INTO users (user_name, full_name, password, account_non_expired, account_non_locked, credentials_non_expired, enabled) VALUES
-  ('weslley', 'Weslley Addson', '19bbf735b27066f2f145e602624e1b24a3fbc54cd5dfd3143fc5feea6bdee9e139ca7332d4806b9f', b'1', b'1', b'1', b'1'),
-  ('matheus', 'Matheus Alves', '19bbf735b27066f2f145e602624e1b24a3fbc54cd5dfd3143fc5feea6bdee9e139ca7332d4806b9f', b'1', b'1', b'1', b'1');
+  ('weslley', 'Weslley Addson', '$2a$10$sl2RuJ8TCx8ZJMSBqFhJXeLom/023VaqQvdXbY0GlR4o0Fi5hpSd.', b'1', b'1', b'1', b'1'),
+  ('matheus', 'Matheus Alves', '$2a$10$sl2RuJ8TCx8ZJMSBqFhJXeLom/023VaqQvdXbY0GlR4o0Fi5hpSd.', b'1', b'1', b'1', b'1');
