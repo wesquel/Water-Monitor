fa6ead2f5b1d348e332aaada8406ab26318f15bb
Weslley Addson <wesquel@hotmail.com>
2023-08-04 13:08:42 -0300

    Correção dos metodos depreciados.

--- water_server/src/main/java/com/water_server/config/SecurityConfig.java
+++ water_server/src/main/java/com/water_server/config/SecurityConfig.java
@@ -4,15 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import com.water_server.security.jwt.JwtConfigurer;
 import com.water_server.security.jwt.JwtTokenProvider;
 
 @EnableWebSecurity
@@ -28,31 +27,18 @@ public class SecurityConfig {
 	}
 
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-	    return http
-			.httpBasic()
-            .disable()
-			.csrf()
-            .disable()
-			.sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-			.and()
-			.authorizeHttpRequests(
-				authorizeHttpRequests -> authorizeHttpRequests
-					.requestMatchers("/auth/signin").permitAll()
-					.requestMatchers("/auth/refresh/**").permitAll()
-					.requestMatchers("/api-docs/**").permitAll()
-					.requestMatchers("/swagger-ui/**").permitAll()
-					.requestMatchers("/v3/api-docs/**").permitAll()
-	                .requestMatchers("/api/**").authenticated()
-	                .requestMatchers("/users").denyAll()
-			)
-			.cors()
-			.and()
-			.apply(new JwtConfigurer(tokenProvider))
-			.and()
-			.build();
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+        .csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(authorize -> authorize
+            .requestMatchers("/dashboard", "/dashboard/**").authenticated()
+            .anyRequest().permitAll()
+        ).rememberMe(Customizer.withDefaults());
+
+        return http.build();
     }
+	
 
 	@Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
