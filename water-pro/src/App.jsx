2ea817343cce4d8f0dcc1aea09ab53eded13b81d
João Pedro <joao.correa@academico.ifpb.edu.br>
2023-08-14 21:46:21 -0300

    add react router dom

--- water-pro/src/App.jsx
+++ water-pro/src/App.jsx
@@ -1,8 +1,9 @@
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
-import Home from "./screens/home";
+import router from "./routes";
 
 function App() {
-  return <Home />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
