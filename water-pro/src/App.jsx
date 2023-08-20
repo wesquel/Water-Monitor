2f5cc7b5a6b551c186cdf24d8ea9abb4b8033488
Filipe Salomão <filipe.salomao22@gmail.com>
2023-08-20 11:14:48 -0300

    Autenticação

--- water-pro/src/App.jsx
+++ water-pro/src/App.jsx
@@ -1,8 +1,61 @@
 import "./App.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./screens/home";
+import Login from "./screens/Login";
+import Register from "./screens/Register";
+import Dashboard from "./screens/Dashboard";
+
+import {
+  AuthProvider,
+  RequireAuth,
+  RequireNoAuth,
+} from "./context/AuthContext";
 
 function App() {
-  return <Home />;
+  return (
+    <div className="App">
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <RequireNoAuth>
+                  <Home />
+                </RequireNoAuth>
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <RequireNoAuth>
+                  <Login />
+                </RequireNoAuth>
+              }
+            />
+            <Route
+              path="/register"
+              element={
+                <RequireNoAuth>
+                  <Register />
+                </RequireNoAuth>
+              }
+            />
+            <Route
+              path="/dashboard"
+              element={
+                <RequireAuth value="/login">
+                  <Dashboard />
+                </RequireAuth>
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </div>
+  );
 }
 
 export default App;
--- water-pro/src/components/Header.jsx
+++ water-pro/src/components/Header.jsx
@@ -1,15 +1,26 @@
 import Container from "./Container";
+import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../assets/logo.svg";
 import Button from "./Button";
+import { useAuthValue } from "../context/AuthContext";
 function Header() {
+  const { user } = useAuthValue();
+  console.log(user);
   return (
     <Container>
       <div className="w-full flex justify-between items-center py-10 flex-col sm:flex-row gap-4">
+        {user && <p>OLA</p>}
         <Logo className="text-mainBlue w-24 h-12" />
-        <div className="flex items-center gap-8">
-          <Button>ENTRAR</Button>
-          <Button variant="outlined">CADASTRAR</Button>
-        </div>
+        {!user && (
+          <div className="flex items-center gap-8">
+            <Link to="/login">
+              <Button>ENTRAR</Button>
+            </Link>
+            <Link to="/register">
+              <Button variant="outlined">CADASTRAR</Button>
+            </Link>
+          </div>
+        )}
       </div>
     </Container>
   );
