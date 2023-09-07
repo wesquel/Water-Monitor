8c6c5413e0d261338f3c503528d458c8506cafdb
Matheus Alves <57298342+Matheus-0@users.noreply.github.com>
2023-09-07 13:35:07 -0300

    Controller de WebSocket desnecessário.

--- water_server/src/main/java/com/water_server/services/WebSocketService.java
+++ water_server/src/main/java/com/water_server/services/WebSocketService.java
@@ -1,9 +1,7 @@
 package com.water_server.services;
 
 import com.water_server.data.MonitorVO;
-import com.water_server.data.SocketMessageVO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
@@ -13,15 +11,7 @@ public class WebSocketService {
     @Autowired
     SimpMessagingTemplate messagingTemplate;
 
-    public ResponseEntity<?> sendMessageToTopic(String topic, SocketMessageVO socketMessageVO) {
-        messagingTemplate.convertAndSend(topic, socketMessageVO);
-
-        return ResponseEntity.ok().build();
-    }
-
-    public ResponseEntity<?> sendMonitorPayload(MonitorVO monitorVO) {
+    public void sendMonitorPayload(MonitorVO monitorVO) {
         messagingTemplate.convertAndSend("/topic/monitor/" + monitorVO.getMACAddress(), monitorVO);
-
-        return ResponseEntity.ok().build();
     }
 }
