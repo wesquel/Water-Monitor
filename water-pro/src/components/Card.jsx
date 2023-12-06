661604f419958014f603928e86ff576eeaa546ed
Matheus Alves <57298342+Matheus-0@users.noreply.github.com>
2023-12-06 18:35:20 -0300

    Fix Card.

--- water-pro/src/components/Card.jsx
+++ water-pro/src/components/Card.jsx
@@ -9,7 +9,7 @@ function Card({ children, className, ...props }) {
         className
       )}
     >
-      <div className="p-2 w-full justify-center flex flex-wrap items-center gap-6 text-mainWhite">
+      <div className="p-2 w-full justify-center flex flex-col items-center gap-6 text-mainWhite">
         {children}
       </div>
     </div>
